cefd859ff6ae747a6d3d7cffcef6778edd5ebe91
chux <chuxoguh@gmail.com>
2023-09-26 21:49:40 +0100

    Portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,91 @@
+import { BiSolidRightArrow } from "react-icons/bi";
+
+const About = () => {
+  const items = [
+    {
+      skill: "REACTJS",
+    },
+    {
+      skill: "REACT NATIVE",
+    },
+    {
+      skill: "REDUX",
+    },
+    {
+      skill: "JAVASCRIPT",
+    },
+    {
+      skill: "NEXTJS",
+    },
+    {
+      skill: "ANIMATIONS",
+    },
+    {
+      skill: "GIT & GITHUB",
+    },
+    {
+      skill: "NPM",
+    },
+    {
+      skill: "TAILWIND CSS",
+    },
+    {
+      skill: "AXIOS/FETCH API",
+    },
+  ];
+
+  return (
+    <div
+      className="flex flex-col items-start justify-center w-full mt-28 px-8 md:px-14 md:mt-40 lg:flex-row lg:px-24 
+    lg:items-center lg:justify-between xl:px-52 xl:mt-72"
+    >
+      <div className="flex flex-col items-start justify-center w-full lg:w-[55%]">
+        <div className="flex items-center justify-start w-full mb-5 md:w-[60%] lg:w-full">
+          <p className="text-[15px] mt-1 md:mt-2 md:text-[19px] lg:text-[20px] xl:mt-1">
+            01.
+          </p>
+          <p className="text-[23px] font-medium ml-2 whitespace-nowrap text-[#feed7a] md:text-[27px] md:font-bold lg:text-[28px]">
+            About Me
+          </p>
+          <hr className="border-[0.01em] border-[#3d3b4b] w-full ml-2" />
+        </div>
+        <div className="md:w-[90%] lg:-[100%]">
+          <p className="font-peralta sm:text-sm text-[12px]"></p>
+        </div>
+      </div>
+
+      <div className="mt-5 md:w-[60%] md:mt-8 lg:w-[40%] lg:mt-0 lg:ml-10">
+        <div className="xl:mb-6">
+          <h1 className="text-[21px] font-bold mb-4 text-[#feed7a] xl:text-[27px]">
+            Skills
+          </h1>
+        </div>
+        <div className="flex flex-wrap items-start justify-between w-full">
+          {items.map((item) => {
+            return (
+              <div
+                key={item.index}
+                className="flex items-center justify-start w-[40%] mb-8"
+              >
+                <BiSolidRightArrow className="text-[#feed7a] text-[12px]" />
+                <p className="text-[12px] text-[#feed7a] ml-2 whitespace-nowrap lg:text-[13px] xl:text-[16px] font-peralta">
+                  {item.skill}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+
+        <a
+          href="#"
+          download
+          className="text-[13px] font-medium text-[#feed7a] underline xl:mt-6 font-peralta"
+        >
+          And a whole lot more
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,85 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { RiTwitterLine } from "react-icons/ri";
+import { SlSocialLinkedin } from "react-icons/sl";
+import logo from "../assets/myLogo.png";
+
+const Contacts = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div
+      data-aos="zoom-in"
+      data-aos-duration="2000"
+      className="flex flex-col items-center justify-center w-full px-8 mt-24 md:px-14 md:mt-36 xl:mt-72"
+    >
+      <div className="flex flex-col items-center justify-start w-full">
+        <p className="text-sm font-light text-[#C4C4C4] lg:text-base xl:text-lg">
+          05. What&apos;s next?
+        </p>
+        <h1 className="text-4xl font-bold text-[#feed7a] my-5 md:my-5 xl:text-5xl">
+          Get In Touch
+        </h1>
+        <p
+          className="text-center text-sm font-light leading-6 md:w-[70%] md:text-base lg:w-[55%] lg:leading-8 xl:w-[45%] xl:text-lg
+                          xl:leading-9"
+        >
+          Have any questions, need help with an exciting project, or you need an
+          awesome developer on your team ?<br />
+          send me an{" "}
+          <a className="text-[#feed7a] ml-1" href="#">
+            Email.
+          </a>
+        </p>
+        <a
+          href="mailto:chuxoguh@gmail.com?subject="
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="h-14 w-32 border-[1px] border-[#feed7a] text-[#feed7a] rounded-md mt-8 xl:w-36">
+            Say Hello
+          </button>
+        </a>
+      </div>
+
+      <div className="flex items-center justify-between w-full mt-20 mb-8 md:hidden">
+        <a
+          href="https://github.com/codechux?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FiGithub className="text-[20px] xl:text-[20px]" />
+        </a>
+        <a href="/" target="_blank" rel="noreferrer">
+          <SlSocialLinkedin className="text-[20px] xl:text-[20px]" />
+        </a>
+        <a href="https://twitter.com/codechux" target="_blank" rel="noreferrer">
+          <RiTwitterLine className="text-[22px] xl:text-[22px]" />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=07061347934&text&type=phone_number&app_absent=0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaWhatsapp className="text-[22px] xl:text-[22px]" />
+        </a>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full mb-2 md:mt-20 lg:mb-3 xl:mb-5 ">
+        <p className="text-xs font-light text-[#feed7a] xl:text-sm">
+          © Stolen Design With Permission
+        </p>
+      </div>
+      <div className="relative"></div>
+      <div className="gradient-01 absolute w-[30%] inset-0 " />
+    </div>
+  );
+};
+
+export default Contacts;
